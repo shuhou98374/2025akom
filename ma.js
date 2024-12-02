3405ebbcefcad5a7669b594c57f47cc80b039ee8
shuhou98374 <154719810+shuhou98374@users.noreply.github.com>
2024-12-02 16:34:06 +0900

    Add ma.js file
    JavaScriptファイルを追加

---
+++ ma.js
@@ -0,0 +1,32 @@
+const loading = document.querySelector('#loading');
+
+window.addEventListener('load', () => {
+  loading.classList.add('loaded');
+});
+
+const items = document.querySelector('.Logo');
+
+const keyframes={
+    opacity:[0,1]
+};
+
+const options={
+    duration:1000,
+    fill:'forwards',
+};
+
+items.animate(keyframes,options);
+
+
+const showLogo =(entries)=>{
+    console.log(entries[0].terget);
+    /*const keyframes = {
+        opacity:[0,1],
+        translate:['200px 0',0],
+    };
+    entries[0].terget.animate(keyframes, 600);*/
+};
+
+const LogoObserver = new IntersectionObserver(showLogo);
+
+LogoObserver.observe(document.querySelector('#taitol'));
